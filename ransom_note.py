11d14e758801b0ebb5915832ef4a303ae41b355f
Melanie Rodriguez <melarodrigg@gmail.com>
2026-05-18 23:24:09 -0600

    Completed lab requirements

--- ransom_note.py
+++ ransom_note.py
@@ -10,4 +10,23 @@ def can_construct(ransomNote: str, magazine: str) -> bool:
     Returns:
         bool: True if ransomNote can be constructed, False otherwise.
     """
-    pass  # TODO: Implement this function
+    
+    note = ""
+    letters = {}
+    
+    for char in magazine:
+        letters[char] = letters.get(char, 0) + 1
+    
+    for char in ransomNote:
+        if letters.get(char, 0) > 0:
+            note += char
+            letters[char] -= 1
+        else:
+            return False
+    
+    return note == ransomNote
+            
+# Poor-man debugging
+print(can_construct("listen", "silent night"))
+print(can_construct("cat", "act one"))
+print(can_construct("robar", "act one"))
